bd6b80a0f579f893cb85c9b79102d2db8e331a86
Hoang Do <hoang.do.2102@gmail.com>
2025-12-08 13:06:41 +0700

    Moon Lander telemetrics params, action params

--- MoonLander/play.py
+++ MoonLander/play.py
@@ -2,19 +2,51 @@ import gymnasium as gym
 import numpy as np
 
 env = gym.make("LunarLander-v3", render_mode="human")
+min_param, max_param = env.observation_space.low, env.observation_space.high
+
+x_range = np.linspace(min_param[0], max_param[0], 10, endpoint=True)
+y_range = np.linspace(min_param[1], max_param[1], 10, endpoint=True)
+
+x_velocity = np.linspace(min_param[2], max_param[2], 10, endpoint=True)
+y_velocity = np.linspace(min_param[3], max_param[3], 10, endpoint=True)
+
+angle = np.linspace(min_param[4], max_param[4])
+angle_vel = np.linspace(min_param[5], max_param[5])
+
+left_leg_contact = min_param[6], max_param[6]
+right_leg_contact = min_param[7], max_param[7]
+
+print("X Range ", x_range)
+print("Y Range ", y_range)
+
+print("X Velocity ", x_velocity)
+print("Y Velocity ", y_velocity)
+
+print("Angle ", angle)
+print("Angle Vel", angle_vel)
+
+print("Left Contact", left_leg_contact)
+print("Right Contact", right_leg_contact)
+
+actions = list(range(env.action_space.n))
+print("ACTION SPACE ", actions)
+
 state, info = env.reset()
-
-print("ENV", env)
-
+total_reward = 0
 max_step = 200
-for step in range(max_step):
-    action = env.action_space.sample()
+
+while True:
+    # action = env.action_space.sample()
+    action = 0
     next_state, reward, terminated, truncated, info = env.step(action)
     state = next_state
-    print("Step ", step)
+    # print("Step ", step, reward)
+    # print("State ", next_state)
+    total_reward += reward
 
     if terminated or truncated:
-        print(f"Reach Final Step")
+        print(total_reward)
+        print(f"Lander Crashed")
         break
 
 env.close()
